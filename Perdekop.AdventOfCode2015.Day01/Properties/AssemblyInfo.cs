fbea73e0e43f528ab3aef2a792399abd72c3b70d
perdekop <perdekop@github.com>
2018-04-17 15:38:01 +0200

    day 1 project setup

---
+++ Perdekop.AdventOfCode2015.Day01/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Perdekop.AdventOfCode2015.Day01")]
+[assembly: AssemblyDescription("http://adventofcode.com/2015/day/1")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Raf Vandeput")]
+[assembly: AssemblyProduct("Perdekop.AdventOfCode2015.Day01")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3177c244-cab0-40e8-819d-053e74d76fd1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Perdekop.AdventOfCode2015/Perdekop.AdventOfCode2015.Day01.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Perdekop.AdventOfCode2015.Day01.Tests")]
+[assembly: AssemblyDescription("http://adventofcode.com/2015/day/1")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Raf Vandeput")]
+[assembly: AssemblyProduct("Perdekop.AdventOfCode2015.Day01.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6532938c-2d28-420b-90cc-aaf1bff09a60")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
